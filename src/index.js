d511b6dd208adc4a6cd06cbd79e62c1315f58e83
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-01-28 18:05:08 -0300

    começo do js e tentativa de algoritmo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
 
+const Algoritmo = (cc) => {
+    const lenght = cc.lenght;
+    let count = 0;
+
+
+for(let i = 0; i < lenght; i++) {
+    let digito = parseInt(cc[i]);
+
+    if ((i+2) % 2 == 0)
+        if((digito *= 2 ) > 9)
+            digito -= 9;
+
+    count += digito;
+}
+return (count % 10) === 0;
+}
+
+const chamar = () => {
+    const cc2 = document.getElementById('cc');
+    let alert = window.alert("")
+
+    if (Algoritmo(cc2.value))
+        alert = "Valido"
+    else
+        alert = "Invalido"
+    
+        cc2.textContent = alert;
+};
+
+
+
+
+
+
 console.log(validator);
