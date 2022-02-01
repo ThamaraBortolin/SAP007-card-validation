19c7a55ab4ac05634a934cbd59b62409c1dbcc8c
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-02-01 11:11:20 -0300

    1.4

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
 import validator from './validator.js';
 
 let cc = document.getElementById('cc').value;
+let botao = document.getElementById('botao');
+botao.addEventListener("click",validar);
 
-const Algoritmo = (cc) => {
-    const lenght = cc.lenght;
-    let count = 0;
+function validar () {
+  let cc1 = document.getElementById('cc').value.length;
+  let cc2 = document.getElementById('cc');
+  let resultado = document.getElementById('nome2');
+  let mensagem = "";
 
+  if(cc1 < 13) {
+    alert ('O número do cartão possui no mínimo 13 dígitos.')
+  
+  }else{
 
-for(let i = 0; i < lenght; i++) {
-    let digito = parseInt(cc[i]);
-
-    if ((i+2) % 2 == 0)
-        if((digito *= 2 ) > 9)
-            digito -= 9;
-
-    count += digito;
-}
-return (count % 10) === 0;
-}
-
-const click = () => {
-    const cc2 = document.getElementById('cc');
-    const resultado = document.getElementById('nome2');
-    let mensagem = "";
-
-if (Algoritmo(cc2.value))
-        mensagem = "Valido";
-    else
-        mensagem = "Invalido";
-    
-        resultado.textContent = mensagem;
-    
-
-    
+  if (validator(cc2.value))
+    mensagem = "Valido";
+  else
+    mensagem = "Invalido";
+  
+    resultado.textContent = mensagem;
+  }
 }
 
+
+
+
     
+  
+
+
+
 console.log(validator);
+
+
+
+
+
+
+
+
+
+    
+ 
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
-const validator = {
-  // ...
-};
+let validator = (cc) => {
+  let length = cc.length;
+  let count = 0;
+
+for(let i=0; i<length;i++) {
+  let digito = parseInt(cc[i]);
+
+if ((i+2) % 2 === 0)
+  if((digito *=2) > 9)
+      digito -= 9;
+
+  count += digito;
+}
+return (count % 10) === 0;
+}
+
+
 
 export default validator;
+
+
