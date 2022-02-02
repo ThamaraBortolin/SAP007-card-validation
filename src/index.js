f839ac1572b639be5c2364fc6ddff1d1dc6d74fc
ThamaraBortolin <thamaralbortolin@gmail.com>
2022-02-02 17:02:01 -0300

    1.5

--- src/index.js
+++ src/index.js
@@ -1,56 +1,48 @@
 import validator from './validator.js';
-
-let cc = document.getElementById('cc').value;
+let nome = document.querySelector("#nome");
+let cc = document.querySelector("#cc");
 let botao = document.getElementById('botao');
-botao.addEventListener("click",validar);
+botao.addEventListener("click",validar);/*evento de click no botao para validar o cartao*/
+nome.addEventListener("keyup", clonarnome);/*eventos de teclar para aparecer no cartao os dados do cliente enquanto ele digita*/
+cc.addEventListener("keyup", clonarcc);
 
-function validar () {
+function clonarnome (){/*fuções para atribuir valor de um input para outro*/
+  let originnome = document.querySelector("#nome").value;
+
+  if (originnome != null){
+    document.querySelector("#clonenome").value = document.querySelector("#nome").value.toUpperCase();
+  }
+}
+
+function clonarcc (){
+  let origincc = document.querySelector("#cc").value;
+  
+  if (origincc != null){
+    document.querySelector("#clonecc").value = document.querySelector("#cc").value;
+  }
+}
+
+
+function validar () { /*função atrelada ao evento de clicl*/
   let cc1 = document.getElementById('cc').value.length;
   let cc2 = document.getElementById('cc');
   let resultado = document.getElementById('nome2');
   let mensagem = "";
-
-  if(cc1 < 13) {
-    alert ('O número do cartão possui no mínimo 13 dígitos.')
+  
+  if(cc1 < 13) { /*condição para ter no minimo 13 digitos no input*/
+    alert ('O número do cartão possui no mínimo 13 dígitos.') 
   
   }else{
 
-  if (validator(cc2.value))
-    mensagem = "Valido";
-  else
-    mensagem = "Invalido";
+  if (validator(cc2.value)){ /*caso há mais que 13 digitos a condição é mostrar se o cartao é valido ou nao*/
+    mensagem = "Válido";
+    console.log('True')
+  }else
+    mensagem = "Inválido";
+    console.log('False')
   
     resultado.textContent = mensagem;
   }
 }
 
-
-
-
-    
-  
-
-
-
 console.log(validator);
-
-
-
-
-
-
-
-
-
-    
- 
-
-
-
-
-
-
-
-
-
-
--- src/validator.js
+++ src/validator.js
@@ -1,17 +1,17 @@
-let validator = (cc) => {
+let validator = (cc) => { /*codigo de luhn*/
   let length = cc.length;
   let count = 0;
 
-for(let i=0; i<length;i++) {
+for(let i=0; i<length;i++) { /* loop para index */
   let digito = parseInt(cc[i]);
 
-if ((i+2) % 2 === 0)
-  if((digito *=2) > 9)
+if ((i) % 2 === 0) /*isolar index de posições pares*/
+  if((digito *=2) > 9)/*condição para multiplicação com numeros decimais*/
       digito -= 9;
 
   count += digito;
 }
-return (count % 10) === 0;
+return (count % 10) === 0;/*modulo da equação para saber se é valido ou seja, um decimal*/
 }
 
 
