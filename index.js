c143736fcf94b1da9077081fedd0f2ae3efb5be4
Thamara Bortolin <thamaralbortolin@gmail.com>
2022-04-04 21:03:35 -0300

    Updates

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ let cc = document.querySelector("#cc");
 let botao = document.getElementById('botao');
 let mes = document.querySelector('#mes');
 let ano = document.querySelector('#ano')
-let github = document.querySelector('#github')
-let linkedin = document.querySelector('#linkedin')
-botao.addEventListener("click", validar); /*eventos trelados as variaveis acima, click é evento do click do cursor e keyup evento de digitar*/
+botao.addEventListener("click", validar,flag); /*eventos trelados as variaveis acima, click é evento do click do cursor e keyup evento de digitar*/
 nome.addEventListener("keyup", clonarnome);
 cc.addEventListener("keyup", clonarcc);
 mes.addEventListener("keyup", data);
 ano.addEventListener("keyup", data);
-github.addEventListener("click", link1)
-linkedin.addEventListener("click", link2)
 
 function clonarnome() {
   /*fuções para atribuir valor de um input para outro. origin, o input em que se insere o valor e clone o que recebe o valor*/
@@ -71,3 +67,75 @@ function validar() {
   }
 
 }
+
+function flag(numbersCard) {
+  let flagCard = ''
+  if (numbersCard.charAt(0) == 4) {
+    flagCard = ' Visa'
+    return flagCard
+  }
+  if (numbersCard.charAt(0) == 3) {
+    if (numbersCard.charAt(0) == 3 && numbersCard.charAt(1) == 4) {
+      flagCard = " American Express"
+      return flagCard
+    } else if (numbersCard.charAt(1) == 6) {
+      flagCard = " Diner's Club / International"
+      return flagCard
+    }
+    else if (numbersCard.charAt(1) == 7) {
+      flagCard = " American Express"
+      return flagCard
+    }
+    else if (numbersCard.charAt(1) == 8) {
+      flagCard = " Diner's Club / Carte Blanche"
+      return flagCard
+    } else if (numbersCard.charAt(1) == 0 && numbersCard.charAt(2) >= 0 &&
+      numbersCard.charAt(2) <= 5) {
+      flagCard = " Diner's Club / Carte Blanche"
+      return flagCard
+    } else {
+      flagCard = " JBC"
+      return flagCard
+    }
+  }
+  if (numbersCard.charAt(0) == 5 && numbersCard.charAt(1) >= 1 &&
+    numbersCard.charAt(1) <= 5) {
+    flagCard = " MasterCard"
+    return flagCard
+  }
+  if (numbersCard.charAt(0) == 6 && numbersCard.charAt(1) == 0 &&
+    numbersCard.charAt(2) == 1 && numbersCard.charAt(3) == 1) {
+    flagCard = " Discover"
+    return flagCard
+  }
+  if (numbersCard.charAt(0) == 1 && numbersCard.charAt(1) == 8 &&
+    numbersCard.charAt(2) == 0 && numbersCard.charAt(3) == 0) {
+    flagCard = " JCB"
+    return flagCard
+  }
+  if (numbersCard.charAt(0) == 2) {
+    if (numbersCard.charAt(1) == 1) {
+      if (numbersCard.charAt(2) == 3 && numbersCard.charAt(3) == 1) {
+        flagCard = ' JCB'
+        return flagCard
+      } else if (numbersCard.charAt(2) == 4 && numbersCard.charAt(3) == 9) {
+        flagCard = " Diner's Club / enRoute"
+        return flagCard
+      }
+    } else if (numbersCard.charAt(1) == 0 && numbersCard.charAt(2) == 1 &&
+      numbersCard.charAt(3) == 4) {
+      flagCard = " Diner's Club / enRoute"
+      return flagCard
+    }
+  }
+  if (numbersCard.charAt(0) == 6 && numbersCard.charAt(1) == 0 && numbersCard.charAt(2) == 6 &&
+    numbersCard.charAt(3) == 2) {
+    flagCard = " HiperCard"
+    return flagCard
+  }
+  flagCard = " sem bandeira"
+  console.log(flagCard)
+  return flagCard
+
+}
+
